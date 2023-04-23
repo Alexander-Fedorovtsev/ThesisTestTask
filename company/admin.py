d7265c09e1036de8a87a31418028d704342f7ef8
Alexander Fedorovtsev <fedorovtsev@mail.ru>
2023-04-23 20:37:50 +0500

    admin site changed

--- company/admin.py
+++ company/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from .models import Employee, Department
 
-admin.site.register(Employee)
-admin.site.register(Department)
+from django.utils.safestring import mark_safe
+
+
+@admin.register(Employee)
+class Employee(admin.ModelAdmin):
+    list_display = ('name', 'position', 'salary', 'age')
+    autocomplete_fields = ('department',)
+    search_fields = ('name', 'position')
+    list_filter = ('department',)
+    readonly_fields = ["preview"]
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.photo.url}">')
+
+
+@admin.register(Department)
+class Department(admin.ModelAdmin):
+    list_display = ('name', 'director')
+    search_fields = ('name', 'director')
+
--- company/models.py
+++ company/models.py
@@ -2,14 +2,29 @@ from django.db import models
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
-    photo = models.ImageField()
-    position = models.CharField(max_length=100)
-    salary = models.FloatField()
-    age = models.IntegerField()
-    department = models.ForeignKey('Department', related_name='employees', on_delete=models.CASCADE)
+    name = models.CharField('ФИО сотрудника', max_length=100)
+    photo = models.ImageField('Фото')
+    position = models.CharField('Должность', max_length=100)
+    salary = models.FloatField('Оклад')
+    age = models.IntegerField('Возраст')
+    department = models.ForeignKey('Department', verbose_name='Департамент', related_name='employees',
+                                   on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return f'{self.name} - {self.position}'
 
 class Department(models.Model):
-    name = models.CharField(max_length=255)
-    director = models.ForeignKey(Employee, related_name='mydepartment', on_delete=models.CASCADE)
+    name = models.CharField('Название', max_length=255)
+    director = models.ForeignKey(Employee, verbose_name='Директор', related_name='mydepartment', blank=True, null=True,
+                                 on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Департамент'
+        verbose_name_plural = 'Департаменты'
+
+    def __str__(self):
+        return f'{self.name}, директор: {self.director.name}'
